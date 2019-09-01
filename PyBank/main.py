bf06cbd73af56d61863f89cedd1eb4dbb8a742b9
Daniel Pimentel <you@example.com>
2019-09-01 16:50:26 -0400

    updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,10 +2,10 @@
 import os
 import csv
 budget_csv = os.path.join("budget_data.csv")
-print("Working")
+#print("Working")
 file = open(budget_csv)
 numline = len(file.readlines())
-print (int(numline - 1))
+print ("The total number of lines is" , int(numline - 1))
 
 #The net total amount of "Profit/Losses" over the entire period
 a = []
@@ -13,19 +13,22 @@ a = []
 with open(budget_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
+    #print(f"Header: {csv_header}")
     for row in csvreader:
         number = int(row[1])
         a.append(number)
        # sum = sum += row[1]
 total = sum(a)
-print(total)
-
-    
+print("The Total is" ,total)
 #The average of the changes in "Profit/Losses" over the entire period
-
-
+def Average(x):
+    print("The Average is" ,x/len(a))
+Average(total)
 #The greatest increase in profits (date and amount) over the entire period
-
+def Max(x): 
+   max_num = max(a)
+   print("The Max is" , max_num)
+Max(total)
+    
 
 #The greatest decrease in losses (date and amount) over the entire period
